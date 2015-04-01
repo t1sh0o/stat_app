8ebdcebc280f8efd06ff10ef4d089ad61de76e8e
t1sh0o <tishoj@gmail.com>
2015-04-01 22:36:30 +0300

    Remove some framework overhead

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,9 @@
 @extends('app')
 
 @section('content')
-<div class="container">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
-
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
+            <h1>Home Page</h1>
 		</div>
 	</div>
-</div>
 @endsection
